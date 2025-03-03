956ceb6783e3eeb39ef413024e8bfb8de707f715
tobiasengberg <tobias.engberg@exilic.se>
2025-03-03 12:11:21 +0100

    Add parameters to search

--- src/pages/Catalogue.jsx
+++ src/pages/Catalogue.jsx
@@ -4,16 +4,47 @@ import { BookList } from "../components/BookList";
 
 export const Catalogue = ({choice}) => {
     const [posts, setPosts] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [title, setTitle] = useState('');
+    const [errorMessage, setErrorMessage] = useState(false);
+    const [searchString, setSearchString] = useState("http://libris.kb.se/xsearch?query=W.V.+Quine&format=json")
 
     useEffect(() => {
-        axios.get("http://libris.kb.se/xsearch?query=W.V.+Quine&format=json")
+        axios.get(searchString)
         .then(response => setPosts(response.data.xsearch.list))
-    }, []);
+    }, [searchString]);
+
+    const handleSearch = () => {
+        let newSearch= "http://libris.kb.se/xsearch?query=";
+        if(author === '' && title === '') {
+            setErrorMessage(true);
+            return;
+        }
+        if(author !== '') newSearch += `förf:(${author.replaceAll(' ', '+')})`;
+        if(title !== '') newSearch += `tit:(${title.replaceAll(' ', '+')})`;
+        newSearch += "&format=json";
+        console.log(newSearch);
+        setSearchString(newSearch);
+    }
 
     return (
         <div>
             {posts !== null && <BookList data={posts} />}
             <h1>Catalogue</h1>
+            <div className="input-card">
+                <label >Author</label>
+                <input 
+                    value={author}
+                    onChange={e => setAuthor(e.target.value)}
+                />
+                              <label >Title</label>
+                <input 
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <button onClick={handleSearch}>Search</button>
+                <p>{errorMessage ? "At least one value must be submitted" : ""}</p>
+            </div>
         </div>
     )
 }
